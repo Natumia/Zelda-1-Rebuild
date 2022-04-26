63ae4f9ce4b16c51e447f854ec46905dfa93e967
Natumia <greatnatumia@gmail.com>
2022-04-25 21:41:47 -0400

    Added a Basic state machine and implemented the foundation for attacking.
    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -3,17 +3,31 @@ extends KinematicBody2D
 var speed = 90
 var velocity = Vector2.ZERO
 
+enum state {
+	IDLE
+	ATTACK
+}
+
+var currentState = state.IDLE
+
 func _ready():
 	pass 
 
 func _physics_process(_delta):
-	get_input()
+	
+	match currentState:
+		state.IDLE:
+			get_input()
+		state.ATTACK:
+			pass
+	
 	velocity= move_and_slide(velocity)
 
-# Basic 4-way movement to replicate Zelda 1. I need to find a way to make it
-# that if the opposite directions are pressed it stops movement.
 func get_input():
 	velocity = Vector2.ZERO
+	
+	# Basic 4-way movement to replicate Zelda 1. I need to find a way to make it
+	# that if the opposite directions are pressed it stops movement.
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 	elif Input.is_action_pressed("ui_right"):
@@ -23,4 +37,27 @@ func get_input():
 			velocity.y -= 1
 		elif Input.is_action_pressed("ui_down"):
 			velocity.y += 1
+	
+	# The start of swinging the sword. It will stop the player from moving, 
+	# by chaning the start to swing. Then after animation ends go back to
+	# a movement state.
+	if Input.is_action_just_pressed("ui_select"):
+		
+		# Creates a wait timer. This is TEMP.
+		var t = Timer.new()
+		t.set_wait_time(0.3)
+		t.set_one_shot(true)
+		self.add_child(t)
+		t.start()
+		
+		velocity = Vector2.ZERO
+		currentState = state.ATTACK
+		print("swing!")
+		
+		# Waits for timer to stop. This is TEMP.
+		yield(t, "timeout")
+		t.queue_free()
+		
+		currentState = state.IDLE
+	
 	velocity = velocity.normalized() * speed
