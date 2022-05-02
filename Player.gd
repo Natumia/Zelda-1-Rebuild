21be58aa20ce7c07cbeea9a366d2168b4a6c2c50
Natumia <greatnatumia@gmail.com>
2022-05-02 13:14:11 -0400

    Added the LinkSprite and AnimationPlayer to match player movement. Link moves!
    Update Test Player.png-4af181d879504cf5dc478ca4661a235a.stex, Z1LinkGreen.png-3099d2f36f85b9d630c772a9fe268cb6.md5, and 7 more files...

--- Player.gd
+++ Player.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 
+# Animation player for the player sprite. I really need to invest time to make
+# this into a tree and blend between the animations. Maybe not for this project.
+onready var animationPlayer = $AnimationPlayer
+
 # These are the variables used to rotate the sword with player movement, and 
 # allow the attack command to make the sword visible.
 onready var sword = $Sword
 onready var swordSprite = $Sword/Sprite
 
-var speed = 90
+var speed = 80
 var velocity = Vector2.ZERO
 
 enum {
@@ -17,14 +21,16 @@ enum {
 var state = IDLE
 
 func _ready():
-	pass 
+	pass
 
 func _physics_process(_delta):
 	
 	match state:
 		IDLE:
+			animationPlayer.stop(false)
 			get_input()
 		MOVE:
+			animationPlayer.play()
 			get_input()
 		ATTACK:
 			pass
@@ -34,23 +40,25 @@ func _physics_process(_delta):
 func get_input():
 	velocity = Vector2.ZERO
 	
-	# Basic 4-way movement to replicate Zelda 1. I need to find a way to make it
-	# that if the opposite directions are pressed it stops movement.
-	# I think I better mimiced how Z1 handles directions. I need to work on
-	# animations so I can better get a feeling of what I'm making.
+	# Basic 4-way movement to replicate Zelda 1. This also rotates the sword
+	# sprite and sets the player animation to match direction.
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 		sword.rotation_degrees = 180
+		animationPlayer.current_animation = "MoveUp"
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 		sword.rotation_degrees = 0
+		animationPlayer.current_animation = "MoveDown"
 	else:
 		if Input.is_action_pressed("ui_left"):
 			velocity.x -= 1
 			sword.rotation_degrees = 90
+			animationPlayer.current_animation = "MoveLeft"
 		elif Input.is_action_pressed("ui_right"):
 			velocity.x += 1
 			sword.rotation_degrees = 270
+			animationPlayer.current_animation = "MoveRight"
 	
 	# The start of swinging the sword. It will stop the player from moving, 
 	# by chaning the start to swing. Then after animation ends go back to
