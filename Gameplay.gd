be5ebac89d2e36f4fafd6e1f5b2167252e22f9c4
Natumia <greatnatumia@gmail.com>
2022-05-07 15:08:36 -0400

    Fixed the pause on gameboot. Now looking for a way to signal to parents when player enters a map instead of the way im doing it now
    Update Gameplay.gd

--- Gameplay.gd
+++ Gameplay.gd
@@ -4,10 +4,11 @@ func _ready():
 	pass
 
 func update_camera(value):
-	var tween = $Camera2D/Tween
-	tween.interpolate_property($Camera2D, "position", $Camera2D.position, value, 1.7, Tween.TRANS_LINEAR)
-	tween.start()
-	get_tree().paused = true
+	if $Camera2D.position != value:
+		var tween = $Camera2D/Tween
+		tween.interpolate_property($Camera2D, "position", $Camera2D.position, value, 1.7, Tween.TRANS_LINEAR)
+		tween.start()
+		get_tree().paused = true
 
-func _on_Tween_tween_completed(object, key):
+func _on_Tween_tween_completed(_object, _key):
 	get_tree().paused = false
